f0d5bf3bdb62508b00f08a77e380b2e288da1fce
Vansh Hedaoo <vanshhedaooprpote@gmail.com>
2024-10-10 23:33:32 +0530

    else 2

---
+++ Chapter 2/If_else2.c
@@ -0,0 +1,16 @@
+// Write a C program to find the maximum between two numbers.
+
+#include<stdio.h>
+
+int main(){
+    int a = 8 , b = 1;
+    if(a>b)
+    {
+        printf("a is greater = %d", a);
+    } 
+    else
+    {
+        printf("b is greater = %d", b);
+    }
+    return 0;
+}
