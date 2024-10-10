85bdd26c29c0a7c8a9991019b2e187d12772f410
Vanshprpote <vanshhedaooprpote@gmail.com>
2024-10-10 19:36:04 +0530

    Add files via upload

---
+++ 01_variables.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main(){
+    int a; // Variable Declaration
+    a = 6; // Variable Initialization
+    printf("%d", a);  // Output is "6"
+    return 0;
+}
+
+// The output of this progam is
+#include<stdio.h>
+
+int main(){
+    int a; // Variable Declaration
+    a = 6; // Variable Initialization
+    printf("The output of this program is %d", a);  // Output is "The output of this program is 6"
+    return 0;
+}
+
+int a
+a = 6  // can also be written as
+int a = 6  // both are valid
