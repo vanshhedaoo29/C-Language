2e95b6f9def9f6bd24c2d97d20203eab2d7bf335
Vansh Hedaoo <vanshhedaooprpote@gmail.com>
2024-10-10 23:25:02 +0530

    else 1

---
+++ Chapter 2/If_else1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+    int i = 10;
+    if(i>15)
+    {
+        printf("i is greater than 15");
+    }
+    else
+    {
+        printf("i is less than 15");
+    }
+    return 0;
+}
