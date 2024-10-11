ddf37978b6d492fcbb0a6c25f57627e04e093af1
Vansh Hedaoo <vanshhedaooprpote@gmail.com>
2024-10-11 09:39:14 +0530

    for loop 1

---
+++ Chapter 2/for_loop1.c
@@ -0,0 +1,13 @@
+// Write a C program to print numbers from 1 to 10.
+
+#include<stdio.h>
+
+int main()
+{
+    int i;
+    for(i = 0; i<10; i++)
+    {
+        printf("%d \n", i+1);
+    }
+    return 0;
+}
