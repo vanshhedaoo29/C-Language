9e13942bddbc5f550fda41d3c8d41c7a3d188223
Vansh Hedaoo <vanshhedaooprpote@gmail.com>
2024-10-13 22:20:10 +0530

    Student Information

---
+++ Chapter 1/student_information.c
@@ -0,0 +1,14 @@
+// Implement a C program to print Student Information using C language
+
+#include<stdio.h>
+
+int main()
+{
+    printf("\t\t\t\t\t\tStudent Information\n");
+    printf("1. Name: Vansh Kishor Hedaoo\n");
+    printf("2. Section: A2\n");
+    printf("3. Year: Ist year CSE\n");
+    printf("4. Address: At Post Warud\n");
+    printf("5. College Name: P.R. Pote Patil College of Engineering and Management, Amravati\n");
+    return 0;
+}
