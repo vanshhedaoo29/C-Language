438f1f7a42ecc39e7ab87703e0aa8f0530b9c304
Vansh Hedaoo <vanshhedaooprpote@gmail.com>
2024-10-13 22:28:28 +0530

    customer info

---
+++ Chapter 1/customer_info.c
@@ -0,0 +1,12 @@
+// Implement customer info. like customer ID, Name, Address, Account No.
+
+#include<stdio.h>
+
+int main()
+{
+    printf("\t\t\t\t\t\tCustomer Information\n");
+    printf("1. Name: Paramveer Singh Oberoi\n");
+    printf("2. Address: 341, Shakuntal Vihar, Amravati\n");
+    printf("3. Account No.: 2345127854\n");
+    return 0;
+}
