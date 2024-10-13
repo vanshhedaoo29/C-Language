2aaf09049a8ce59f7c4f29ac49bb9e993eee7427
Vansh Hedaoo <vanshhedaooprpote@gmail.com>
2024-10-13 22:54:29 +0530

    5

---
+++ Chapter 1/getchar_1.c
@@ -0,0 +1,12 @@
+// The following program reads a single key into a char variable
+
+#include<stdio.h>
+
+int main()
+{
+    char ch;
+    printf(" Enter a Character: ");
+    ch = getchar();
+    putchar(ch);
+    return 0;
+}
