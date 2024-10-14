698ac81299527b57572d72675a314233f39ba0e7
Vansh Hedaoo <vanshhedaooprpote@gmail.com>
2024-10-14 09:43:42 +0530

    10

---
+++ Chapter 2/do_while_loop1.c
@@ -0,0 +1,15 @@
+// Write a C program to print numbers from 1 to 10
+
+#include<stdio.h>
+
+int main()
+{
+    int i = 1;
+    do
+    {
+        printf("%d \n", i );
+        i++;
+    }
+    while(i<=10);
+    return 0;
+}
