97cbba9cd236fe8dd3a063d3af5cb54049003915
Vansh Hedaoo <vanshhedaooprpote@gmail.com>
2024-12-18 08:03:03 +0530

    4

---
+++ Practicals /Practical4.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() 
+{
+    int a, b, i, n, sum=0;
+    printf("Enter the series length : ");
+    scanf("%d", &n);
+    
+    printf("Enter the value of a : ");
+    scanf("%d", &a);
+    
+    printf("Enter the value of b : ");
+    scanf("%d", &b);
+    
+    printf("%d\t%d\t", a, b);
+    for(i=2 ; i<n ; i++)
+    {
+        sum = a + b;
+        printf("%d\t", sum);
+        a = b;
+        b = sum;
+    }
+    return 0;
+}
+    
+ 
